0b15b6d4d14c807e7b3e0fd832c7e2bd7aa5e437
yunus <yunus@DESKTOP-RNAQVAA>
2023-07-04 16:07:01 +0300

    Add project files.

---
+++ Presentation/Controllers/BooksController.cs
@@ -0,0 +1,122 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IServiceManager _manager;
+
+        public BooksController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            try
+            {
+                var books = _manager.BookService.GetAllBooks(false);
+
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var book = _manager
+                    .BookService
+                    .GetOneBookById(id, false);
+
+                if (book is null)
+                    return NotFound();
+
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book is null)
+                    return BadRequest();
+
+                _manager.BookService.CreateOneBook(book);
+
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
+        {
+            try
+            {
+                if (book is null)
+                    return BadRequest();
+
+                //if (id != book.Id)
+                //    return BadRequest(new
+                //    {
+                //        statusCode = 400,
+                //        message = $"{id} must match with body's id!"
+                //    });
+
+                _manager.BookService.UpdateOneBook(id, book, true);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+
+                _manager.BookService.DeleteOneBook(id, false);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
