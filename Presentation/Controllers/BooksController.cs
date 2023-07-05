7e15ad9799e88d9fef2519cff79a34e8c1795928
yunus <yunus@DESKTOP-RNAQVAA>
2023-07-05 20:32:40 +0300

    BookDtoForUpdate class has been added for Update operation by using AutoMapper package

--- Presentation/Controllers/BooksController.cs
+++ Presentation/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Exceptions;
+﻿using Entities.DataTransferObjects;
+using Entities.Exceptions;
 using Entities.Models;
 using Microsoft.AspNetCore.Mvc;
 using Services.Contracts;
@@ -51,12 +52,12 @@ namespace Presentation.Controllers
         }
 
         [HttpPut("{id:int}")]
-        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] BookDtoForUpdate bookDto)
         {
-            if (book is null)
+            if (bookDto is null)
                 return BadRequest();
 
-            _manager.BookService.UpdateOneBook(id, book, true);
+            _manager.BookService.UpdateOneBook(id, bookDto, true);
 
             return NoContent();
 
